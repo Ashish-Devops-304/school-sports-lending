9535e62dde3050abfab4a677061ebdb459ca15b9
Ashish-Devops-304 <304ashishyadav@gmail.com>
2025-11-14 20:13:36 +0530

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,7 +9,6 @@ class LoginScreen extends StatefulWidget {
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
-
 class _LoginScreenState extends State<LoginScreen> {
   final _email = TextEditingController();
   final _pass = TextEditingController();
